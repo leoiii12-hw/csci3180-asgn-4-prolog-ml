51bfde687f1404877b3de295b11127e6541e2623
Leo Choi <choimankin@gmail.com>
2018-04-15 23:25:08 +0800

    - Added headers - Removed idea files

--- logic.pl
+++ logic.pl
@@ -1,3 +1,21 @@
+% /*
+%  * CSCI3180 Principles of Programming Languages
+%  *
+%  * --- Declaration ---
+%  *
+%  * I declare that the assignment here submitted is original except for source
+%  * material explicitly acknowledged. I also acknowledge that I am aware of
+%  * University policy and regulations on honesty in academic work, and of the
+%  * disciplinary guidelines and procedures applicable to breaches of such policy
+%  * and regulations, as contained in the website
+%  * http://www.cuhk.edu.hk/policy/academichonesty/
+%  *
+%  * Assignment 4
+%  * Name : Choi Man Kin
+%  * Student ID : 1155077469
+%  * Email Addr : mkchoi6@cse.cuhk.edu.hk
+%  */
+
 % Assume the set of natural numbers consists of zero
 
 % 1a
